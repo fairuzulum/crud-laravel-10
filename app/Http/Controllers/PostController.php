24232424c1b711469520994ae259d5bc358e1a2b
fairuzulum <usleunmdi32@gmail.com>
2023-11-16 14:57:26 +0700

    insert data

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
 
 class PostController extends Controller
 {
@@ -14,5 +15,30 @@ class PostController extends Controller
 
         return view('posts.index', compact('posts'));
 
+    }
+
+    public function create(): View
+    {
+        return view("posts.create");
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $this->validate($request, [
+            'image' => 'required|image|mimes:jpeg,jpg,png|max:2048',
+            'title' => 'required|min:5',
+            'content' => 'required|min:10'
+        ]);
+
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+
+        Post::create([
+            'image' => $image->hashName(),
+            'title' => $request->title,
+            'content' => $request->content
+        ]);
+
+        return redirect()->route('posts.index')->with(['success' => 'Data berhasil disimpan']);
     }
 }
